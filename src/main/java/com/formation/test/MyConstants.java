30c461b18b2d2f67099280f76b20ba73f31aba56
Senpothia <kintelalpz@gmail.com>
2020-04-11 18:47:37 +0200

    Init

---
+++ src/main/java/com/formation/test/MyConstants.java
@@ -0,0 +1,15 @@
+package com.formation.test;
+
+
+public class MyConstants {
+ 
+    // Replace with your email here:  
+    public static final String MY_EMAIL = "kintelalpz@gmail.com";
+ 
+    // Replace password!!
+    public static final String MY_PASSWORD = "Zaragoza1971";
+ 
+    // And receiver!
+    public static final String FRIEND_EMAIL = "caroban.2014@gmail.com";
+ 
+}
